232793eb5b8455043313f09bc0756729035edf7c
lyj <632677663@qq.com>
2015-12-09 16:27:27 +0800

    项目的提交.

--- a/src/a/ad.java
+++
@@ -1,8 +0,0 @@
-package a;
-
-public class ad {
-    public static void main(String[] args) {
-        System.out.println("a");
-        System.out.println("b");
-    }
-}
---
+++ nagel/src/main/java/nagel/frame/category/util/DateHelper.java
@@ -0,0 +1,21 @@
+package nagel.frame.category.util;
+
+import java.util.Date;
+
+/**
+ * 项目名称 : nagel
+ * 创建日期 : 2015年12月8日
+ * 类  描  述 : 日期工具类
+ * 修改历史 : 
+ */
+public class DateHelper {
+    
+    public static String convertToString(String style,Date data){
+        
+        
+        
+        return null;
+        
+    }
+    
+}
